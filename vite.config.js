ea448a3e3fbb6a311dcb9c7f663f62ee553fb45d
Talion-101 <isurupromodh@gmail.com>
2025-07-20 22:58:30 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/futuristic-doc-search/', // 👈 MUST match your repo name
+  base: '/futuristic-doc-search/', // ← must match your repo name exactly
 });
